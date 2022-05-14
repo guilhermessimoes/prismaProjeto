1e9ba5b6f0f1aca83a2b2ce8eeef9fd2d9b1ed6a
Guilherme Simoes <guilhermejunios@gmail.com>
2022-05-13 23:24:51 -0300

    Cadastro de entregas

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateClientController } from './modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router()
@@ -12,10 +13,13 @@ const createClientController =  new CreateClientController();
 const authenticateClientController =  new AuthenticateClientController();
 const createDeliverymanController =  new CreateDeliverymanController();
 const authenticateDeliverymanController =  new AuthenticateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post('/client/authenticate', authenticateClientController.handle);
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 routes.post('/client/', createClientController.handle);
 routes.post('/deliveryman/', createDeliverymanController.handle);
+routes.post('/delivery/', createDeliveryController.handle);
+
 
 export { routes }
